5b657bb75c011c25d2ef29689ba7dbee29307295
Rich Chatterton <rich.m.chatterton@gmail.com>
2018-05-31 17:16:05 -0500

    Adds unit tests for loading the Organization model from AWS

--- tests/unit/models/aws_entities/test_organization.py
+++ tests/unit/models/aws_entities/test_organization.py
@@ -156,3 +156,100 @@ class TestOrganization(unittest.TestCase):
         problems = org.validate()
         helpers.print_expected_actual_diff(expected_problems, problems)
         assert expected_problems == problems
+
+    @mock.patch('cumulogenesis.models.aws_entities.Organization.raise_if_invalid')
+    @mock.patch('cumulogenesis.models.aws_entities.Organization.initialize_aws_model')
+    #pylint: disable=unused-argument
+    def test_dry_run(self, initialize_mock, raise_mock):
+        '''
+        Test Organization.dry_run
+        '''
+        provisioner_overrides = {"profile": "foo"}
+        provisioner = {"role": "bar"}
+        expected_provisioner = {"profile": "foo", "role": "bar"}
+        expected_report = {"actions": {}}
+        with mock.patch('cumulogenesis.models.aws_entities.Organization.validate') as validate_mock:
+            validate_mock.return_value = {"organization": "some_problem"}
+            expected_compare_report_arg = {"aws_model_problems": {"organization": "some_problem"}}
+            with mock.patch('cumulogenesis.models.aws_entities.Organization.compare_against_aws_model') as compare_mock:
+                compare_mock.return_value = {"actions": {}}
+                org_mock = self._get_base_organization()
+                org_mock.aws_model = self._get_base_organization()
+                org_mock.provisioner = provisioner
+                report = org_mock.dry_run(provisioner_overrides=provisioner_overrides)
+                helpers.print_expected_actual_diff(expected_report, report)
+                compare_mock.assert_called_with(report=expected_compare_report_arg)
+                assert expected_report == report
+                helpers.print_expected_actual_diff(expected_provisioner, provisioner)
+                assert org_mock.provisioner == expected_provisioner
+
+    @staticmethod
+    def _get_mock_org_service_instance():
+        org_service_instance = mock.Mock
+        org_service_instance.load_organization = mock.Mock()
+        org_service_instance.load_accounts = mock.Mock()
+        org_service_instance.load_orgunits = mock.Mock()
+        org_service_instance.load_policies = mock.Mock()
+        org_service_instance.load_organization.return_value = None
+        org_service_instance.load_accounts.return_value = None
+        org_service_instance.load_orgunits.return_value = None
+        org_service_instance.load_policies.return_value = None
+        return org_service_instance
+
+    @mock.patch('cumulogenesis.models.aws_entities.Organization._get_session_builder')
+    @mock.patch('cumulogenesis.models.aws_entities.organization.OrganizationService')
+    #pylint: disable=unused-argument
+    def test_load_not_exists(self, org_service_mock, session_builder_mock):
+        '''
+        Test Organization.load when the organization does not exist
+
+        When Organization.exists is False after calling OrganizationService.load_organization,
+        it should not attempt to load the rest of the Organization entities.
+        '''
+        org_mock = self._get_base_organization()
+        org_service_instance = mock.Mock
+        org_mock.source = "aws"
+        org_mock.exists = False
+        org_service_instance = self._get_mock_org_service_instance()
+        org_service_mock.return_value = org_service_instance
+        org_mock.provisioner = {"profile": "foo"}
+        org_mock.load()
+        org_service_instance.load_organization.assert_called()
+        org_service_instance.load_accounts.assert_not_called()
+        org_service_instance.load_orgunits.assert_not_called()
+        org_service_instance.load_policies.assert_not_called()
+
+    @mock.patch('cumulogenesis.models.aws_entities.Organization._get_session_builder')
+    @mock.patch('cumulogenesis.models.aws_entities.organization.OrganizationService')
+    #pylint: disable=unused-argument
+    def test_load_exists(self, org_service_mock, session_builder_mock):
+        '''
+        Test Organization.load when the organization exists
+
+        When Organization.exists is True after calling OrganizationService.load_organization,
+        it should not attempt to load the rest of the Organization entities.
+        '''
+        org_mock = self._get_base_organization()
+        org_service_instance = mock.Mock
+        org_mock.source = "aws"
+        org_mock.exists = True
+        org_service_instance = self._get_mock_org_service_instance()
+        org_service_mock.return_value = org_service_instance
+        org_mock.provisioner = {"profile": "foo"}
+        org_mock.load()
+        org_service_instance.load_organization.assert_called()
+        org_service_instance.load_accounts.assert_called()
+        org_service_instance.load_orgunits.assert_called()
+        org_service_instance.load_policies.assert_called()
+
+    def test_load_not_aws(self):
+        '''
+        Test Organization.load when Organization source is not "aws"
+
+        When the "source" attribute on the Organization on which load is called is
+        not AWS, it should raise NotAwsModelException.
+        '''
+        org_mock = self._get_base_organization()
+        org_mock.source = "config"
+        with self.assertRaises(exceptions.NotAwsModelException):
+            org_mock.load()
